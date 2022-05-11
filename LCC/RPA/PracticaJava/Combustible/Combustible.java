1a8f7610c14a128fe7a0165234e7b6214a02cb04
Santino Fuentes <sang@debian.com>
2022-05-11 17:43:15 -0300

    <Combustible>

---
+++ LCC/RPA/PracticaJava/Combustible/Combustible.java
@@ -0,0 +1,18 @@
+/*
+* RPA, TP # 3.2.
+*
+* Ejercicio # 4 <Combustible>.
+*
+* Notación UpperCammelCase, Indentación 2, Java. 
+*
+* Santino Fuentes, 2022.
+******************************************************************************/
+
+
+import java.util.Scanner;
+
+public class Combustible
+
+{
+	public static void main (String[] args)
+ 
