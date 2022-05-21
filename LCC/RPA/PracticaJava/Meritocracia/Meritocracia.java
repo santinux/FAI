5c9429a3e376b7f4ed7942b92674c7a33c005aa5
Santino Fuentes <santinofuentes24.sf@gmail.com>
2022-05-21 02:11:39 -0300

    <meritocracia>

--- LCC/RPA/PracticaJava/Meritocracia/Meritocracia.java
+++ LCC/RPA/PracticaJava/Meritocracia/Meritocracia.java
@@ -7,24 +7,17 @@
 *
 * Santino Fuentes, 2022.
 ******************************************************************************/
+/*
+* El programa define el valor de un bono a cobrar por la persona ingresada,
+* y si esta está habilitada para recibirlo.
+*/
 
 import java.util.Scanner;
-
 public class Meritocracia
-
 {
-
 	// Programa principal
-
 	public static void main (String[] args)
-
-	/*
-	* El programa define el valor de un bono a cobrar por la persona ingresada,
-	* y si esta está habilitada para recibirlo
-	*/
-
 	{
-
 		int Inasistencias;
 		double Bono;
 		String Nombre;
@@ -34,28 +27,17 @@ public class Meritocracia
 		Nombre = sc.next();
 		System.out.println("Ingrese la cantidad de inasistencias: ");
 		Inasistencias = sc.nextInt();
-
 		if(Inasistencias <= 2)
-
 		{
-
 			Bono = 15000 - (Inasistencias * 500);
 			System.out.println("A " + Nombre + 
 			" le corresponde cobrar el bono por buena asistencia.\n" + 
 			"Monto especial a percibir: $" + Bono + ".\n");
-
 		}
-
 		else
-
 		{
-
 			System.out.println("A " + Nombre + 
-			" no le corresponde cobrar el bono por buena asistencia.\n" + 
-			"JAJAJA, tonta.\n");
-
+			" no le corresponde cobrar el bono por buena asistencia.\n");
 		}
-
 	}
-
 }
